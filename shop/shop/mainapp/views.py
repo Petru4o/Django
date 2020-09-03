48e1bdad71c2811abcd83a5f2f8591f74e4e1038
Petro Salii <saliypetro22@gmail.com>
2020-09-02 22:00:45 -0700

    Main page add content

--- shop/shop/mainapp/views.py
+++ shop/shop/mainapp/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Notebook,Smartphone,Category
-
-# Create your views here.
-def test_view(request):
-    categories = Category.objects.get_categories_for_left_sidebar()
-    return render(request, 'base.html', {'categories': categories})
+from django.views.generic import DetailView, View
+from .models import Notebook, Smartphone, Category, LatestProducts
+from .mixins import CategoryDetailMixin
 
 
-class ProductDetailView(DetailView):
+class BaseView(View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        products = LatestProducts.objects.get_products_for_main_page(
+            'notebook', 'smartphone'
+        )
+        context = {
+            'categories': categories,
+            'products': products
+        }
+        return render(request, 'base.html', context)
+
+
+class ProductDetailView(CategoryDetailMixin, DetailView):
     CT_MODEL_CLASS = {
         'notebook': Notebook,
         'smartphone': Smartphone
@@ -23,8 +33,8 @@ class ProductDetailView(DetailView):
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
 
-class CategoryDetailView(DetailView):
 
+class CategoryDetailView(CategoryDetailMixin, DetailView):
     model = Category
     queryset = Category.objects.all()
     context_object_name = 'category'
