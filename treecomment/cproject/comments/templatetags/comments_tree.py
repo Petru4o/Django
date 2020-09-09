917198b04c71e25bd69774f82c4a48e08403f180
Petro Salii <saliypetro22@gmail.com>
2020-09-09 00:30:06 -0700

    Part-1 HTML

--- treecomment/cproject/comments/templatetags/comments_tree.py
+++ treecomment/cproject/comments/templatetags/comments_tree.py
@@ -1,23 +1,35 @@
 from django.template import Library
 from django.utils.html import mark_safe
 
-
 register = Library()
+
 
 @register.filter
 def comments_filter(comments_list):
     res = """
-        <ul>
-        {}
+        <ul style='list-style-type:none;'>
+            <div class="col-md-12 mt-2">
+                {}
+            </div>
         </ul>
         """
     i = ""
     for comment in comments_list:
         i += """
             <li>
-            {comment_id}
+                <div class="col-md-12 mt-2 p-0 mb-2">
+                    <small>{author}</small> | Date: {timestamp}
+                    <hr>
+                    <p>{text} | id={id}</p>
+                    <a href="#" class="reply" data-id="{id}" data-parent={parent_id}>Answer</a>
+                    <form action="" method="POST" class="comment-form form-group" id="form-{id}" style="display:none;">
+                        <textarea type="text" class="form-control" name="comment-text"></textarea><br>
+                        <input type="submit" class="btn btn-primary submit-reply" data-id="{id}" data-submit-reply="{parent_id}" value="Send"></input>
+                    </form>
+                </div>
             </li>
-            """.format(comment_id=comment['id'])
+            """.format(id=comment['id'], author=comment['author'], timestamp=comment['timestamp'],
+                       parent_id=comment['parent_id'], text=comment['text'])
         if comment.get('children'):
             i += comments_filter(comment['children'])
     return mark_safe(res.format(i))
--- treecomment/cproject/comments/views.py
+++ treecomment/cproject/comments/views.py
@@ -1,8 +1,27 @@
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Post
+from .forms import CommentForm
 from .utils import create_comments_tree
 
 def base_view(request):
     comments = Post.objects.first().comments.all()
     result = create_comments_tree(comments)
+    comment_form = CommentForm(request.POST or None)
     return render(request, 'base.html', {'comments': result})
+
+
+def create_comment(request):
+    comment_form = CommentForm(request.POST or None)
+    if comment_form.is_valid():
+        new_comment = comment_form.save(commit=False)
+        new_comment.user = request.user
+        new_comment.text = comment_form.cleaned_data['text']
+        new_comment.content_type = ContentType.objects.get(model='post')
+        new_comment.object_id = 1
+        new_comment.parent = None
+        new_comment.is_child = False
+        new_comment.save()
+    return HttpResponseRedirect('/post-comment')
+
